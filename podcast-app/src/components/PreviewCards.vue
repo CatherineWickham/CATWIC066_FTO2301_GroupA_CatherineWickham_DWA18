85684427e45ea440300e62174dfd714506183394
Catherine Wickham <cathhwickham@gmail.com>
2023-07-19 14:00:00 +0200

    #2 Base project & login system

---
+++ podcast-app/src/components/PreviewCards.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container class="fill-height">
+    <v-card
+      width="400"
+      title="This is a title 1"
+      subtitle="This is a subtitle"
+      text="This is content"
+    ></v-card>
+
+    <v-card
+      width="400"
+      title="This is a title 2"
+      subtitle="This is a subtitle"
+      text="This is content"
+    ></v-card>
+
+    <v-card
+      width="400"
+      title="This is a title 3"
+      subtitle="This is a subtitle"
+      text="This is content"
+    ></v-card>
+    
+    <v-card
+      width="400"
+      title="This is a title 4"
+      subtitle="This is a subtitle"
+      text="This is content"
+    ></v-card>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style>
+.v-card{
+  margin: 1rem;
+}
+</style>
---
+++ podcast-app/src/views/FavoritesView.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Favorites Page {{ account !== undefined ? `for
+  ${account.data.session.user.email}` : ""}}</h1>
+  <PreviewCards />
+</template>
+
+<script setup>
+  import PreviewCards from '@/components/PreviewCards.vue'
+  import { supabase } from '@/clients/supabase';
+  import { ref } from 'vue'
+
+  const account = ref()
+
+  const getSession = async () => {
+    account.value = await supabase.auth.getSession()
+  }
+
+  getSession()
+
+</script>
